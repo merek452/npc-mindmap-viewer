f847d0b1e443fd6a14a682620b923ba4ca7df4c6
Kerem Bray <kbray@wearoptimo.com>
2026-01-12 15:44:46 +1000

    Fix map tab not appearing - update second switchTab function to include map tab

--- generate_mindmap.py
+++ generate_mindmap.py
@@ -2946,8 +2946,9 @@ class NPCRelationshipMapper:
             let tabIndex = -1;
             if (tabName === 'cards') tabIndex = 0;
             else if (tabName === 'mindmap') tabIndex = 1;
-            else if (tabName === 'editor') tabIndex = 2;
-            else if (tabName === 'inventory') tabIndex = 3;
+            else if (tabName === 'map') tabIndex = 2;
+            else if (tabName === 'editor') tabIndex = 3;
+            else if (tabName === 'inventory') tabIndex = 4;
             
             if (tabIndex >= 0 && tabs[tabIndex]) {
                 tabs[tabIndex].classList.add('active');
